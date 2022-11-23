588142f3784e01b66d52ee433c5c3937fd46fd7d
Thorium0 <46021061+Thorium0@users.noreply.github.com>
2022-11-23 08:18:43 +0100

    Delete keys.py

--- keys.py
+++
@@ -1,8 +0,0 @@
-SECRET_KEY = 'B0amperhDKKJaQPS3trlKXFgenq243h567w5g635owQ2PF'
-
-SQL_USER = 'root'
-SQL_PASSWORD = '0786459123'
-
-# use 'mssql' for windows-server
-SQL_BACKEND = 'django.db.backends.mysql' 
-SQL_HOST = '127.0.0.1'
